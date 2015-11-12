1082d44c88f1d5effe8bb2f7f15cc208fe1122d3
Kevin Aujla <kevinaujla@gmail.com>
2015-11-11 19:38:25 -0800

    [feat] add alert and loading services

---
+++ client/services/alertServices.js
@@ -0,0 +1,20 @@
+/*
+
+alertServices.js
+file to share alert services between controllers
+
+*/
+
+angular.module('App.loadingServices', [])
+
+.factory("success", function(message) {
+
+  '<div class="alert"><a class="close" data-dismiss="alert">×</a><span>' + message + '</span></div>')
+
+})
+
+.factory("failure", function(message) {
+
+  '<div class="alert"><a class="close" data-dismiss="alert">×</a><span>' + message + '</span></div>')
+
+})
---
+++ client/services/loadingService.js
@@ -0,0 +1,16 @@
+/*
+
+loadingServices.js
+file to share loading alert services between controllers
+
+*/
+
+angular.module('App.alertServices', [])
+
+.factory("open", function(message) {
+
+})
+
+.factory("close", function(message) {
+
+})
