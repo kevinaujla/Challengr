255e13d1328bc644104df65d20976025eb1b3e60
Kevin Aujla <kevinaujla@gmail.com>
2015-11-11 17:54:11 -0800

    [feat] add charity controller and view

--- client/app.js
+++ client/app.js
@@ -7,13 +7,18 @@ module dependency injections and route configurations
 
 angular.module('App', [
   'ngRoute',
-  'App.newsFeed'
-  ]).
-config(['$routeProvider', function($routeProvider) {
+  'App.newsFeed',
+  'App.charity'
+  ])
+.config(['$routeProvider', function($routeProvider) {
  $routeProvider
   .when('/newsFeed', {
     templateUrl: 'newsFeed/newsFeed.html',
     controller: 'newsFeedCtrl'
+  })
+   .when('/charity', {
+    templateUrl: 'charity/charity.html',
+    controller: 'charityCtrl'
   })
   .otherwise({redirectTo: '/home'});
 }]);
---
+++ client/charity/charity.js
@@ -0,0 +1,12 @@
+/*
+
+charity.js
+sets up charity controller
+
+*/
+
+angular.module('App.charity', ['ngRoute'])
+
+.controller('charityCtrl', [function() {
+
+}]);
