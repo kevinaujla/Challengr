4da6f61f334113d62e6826f2861caef6bd42e221
Jordan Winkelman <jordanwink201@gmail.com>
2015-11-12 15:37:56 -0800

    [feat] : add protractor config setup

---
+++ test/client/test.js
@@ -0,0 +1,14 @@
+/*
+
+  Test
+  
+
+*/
+
+describe('Protractor Demo App', function() {
+  it('should have a title', function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
+});
