b117e5e77032c2403b8291ab77472a2bb0ed174e
arthur <arthur.mathies@googlemail.com>
2015-11-11 17:29:34 -0800

    [feat]: create basic layout api endpoint signup/signin

--- server/server.js
+++ server/server.js
@@ -23,3 +23,23 @@ app.use(express.static(__dirname + "/../client"));
 
 app.listen(port);
 console.log('Challengr is listening on port ', port);
+
+app.post("/api/signup", function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  // check if the user exists in the database
+  // if yes let the user know that the username already exists
+  // else hash the password store the user to the database and return a token 
+
+});
+
+
+app.post("/api/signin", function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  // check if the user exists in the database
+  // if yes log the user in and return a token
+  // else let the user know that he probably put in a wrong password or username
+});
