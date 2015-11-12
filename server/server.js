4f5be63d291deb183000cb647559785a01006f69
Robert <robert.m.velasco@gmail.com>
2015-11-11 18:16:28 -0800

    [feat]: setup basic server.js, serves static resources

---
+++ server/server.js
@@ -0,0 +1,26 @@
+/*
+
+server.js
+express/node server 
+
+*/
+
+var express = require('express');
+var morgan = require('morgan');
+var request = require('request');
+var session = require('express-session');
+
+var path = require('path');
+
+var Promise = require('bluebird');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(morgan('dev')); 
+
+app.use(express.static(__dirname + "/../client"));
+
+
+
+app.listen(port);
+console.log('Challengr is listening on port ', port);
