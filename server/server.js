d4102850bced505d25f0dd962b9389bb07919bc3
arthur <arthur.mathies@googlemail.com>
2015-11-11 18:26:12 -0800

    [refactor]: add comments

--- server/server.js
+++ server/server.js
@@ -6,24 +6,31 @@ express/node server
 */
 
 var express = require('express');
+// logging requests to the server
 var morgan = require('morgan');
+// convenience method for making http requests
 var request = require('request');
 
+// module for transforming filepaths
 var path = require('path');
 
+// promise library to avoid callback hell
 var Promise = require('bluebird');
+// create express app
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(morgan('dev')); 
 
+// serving static files from client folder
 app.use(express.static(__dirname + "/../client"));
 
 
-
+// start server to listen on localhost:port
 app.listen(port);
 console.log('Challengr is listening on port ', port);
 
+// API endpoint for signup requests
 app.post("/api/signup", function(req, res) {
   var username = req.body.username;
   var password = req.body.password;
@@ -31,10 +38,9 @@ app.post("/api/signup", function(req, res) {
   // check if the user exists in the database
   // if yes let the user know that the username already exists
   // else hash the password store the user to the database and return a token 
-
 });
 
-
+// API endpoint for signin requests
 app.post("/api/signin", function(req, res) {
   var username = req.body.username;
   var password = req.body.password;
