daaf5c2fce0f9850a68a360491067e760a50c112
Kevin Aujla <kevinaujla@gmail.com>
2015-11-13 11:46:42 -0800

    [feat] implemented ui-router to app

--- client/app.js
+++ client/app.js
@@ -6,76 +6,78 @@ module dependency injections and route configurations
 */
 
 angular.module('App', [
-    'ngRoute',
-    'App.auth',
-    'App.profile',
-    'App.newsFeed',
-    'App.charity',
-    'App.challenge',
-    'App.alertService',
-    'App.loadingService',
-    'App.alertDirective',
-    'App.launch'
+  'ui.router',
+  'App.auth',
+  'App.profile',
+  'App.newsFeed',
+  'App.charity',
+  'App.challenge',
+  'App.alertService',
+  'App.loadingService',
+  'App.alertDirective',
+  'App.launch'
   ])
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .when('/signin', {
-        templateUrl: 'script/module/user/auth/signin.html',
-        controller: 'authCtrl',
-        controllerAs: 'authCtrl',
-        data: {
-          authenticate: false
-        }
-      })
-      .when('/signup', {
-        templateUrl: 'script/module/user/auth/signup.html',
-        controller: 'authCtrl',
-        controllerAs: 'authCtrl',
-        data: {
-          authenticate: false
-        }
-      })
-      .when('/profile', {
-        templateUrl: 'script/module/user/auth/profile.html',
-        controller: 'profileCtrl',
-        controllerAs: 'profileCtrl',
-        data: {
-          authenticate: true
-        }
-      })
-      .when('/newsFeed', {
-        templateUrl: 'script/module/newsFeed/newsFeed.html',
-        controller: 'newsFeedCtrl',
-        controllerAs: 'newsFeedCtrl',
-        data: {
-          authenticate: true
-        }
-      })
-      .when('/charity', {
-        templateUrl: 'script/module/charity/charity.html',
-        controller: 'charityCtrl',
-        controllerAs: 'charityCtrl',
-        data: {
-          authenticate: true
-        }
-      })
-      .when('/challenge', {
-        templateUrl: 'script/module/challenge/challenge.html',
-        controller: 'challengeCtrl',
-        controllerAs: 'challengeCtrl',
-        data: {
-          authenticate: true
-        }
-      })
-      .when('/launch', {
-        templateUrl: 'script/module/challenge/launch.html',
-        controller: 'launchCtrl',
-        controllerAs: 'launchCtrl',
-        data: {
-          authenticate: true
-        }
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+
+.config(['$stateProvider','$urlRouterProvider' function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise({
+    redirectTo: '/'
+  });
+
+  $stateProvider
+  .state('/signin', {
+    templateUrl: 'script/module/user/auth/signin.html',
+    controller: 'authCtrl',
+    controllerAs: 'authCtrl',
+    data: {
+      authenticate: false
+    }
+  })
+  .state('/signup', {
+    templateUrl: 'script/module/user/auth/signup.html',
+    controller: 'authCtrl',
+    controllerAs: 'authCtrl',
+    data: {
+      authenticate: false
+    }
+  })
+  .state('/profile', {
+    templateUrl: 'script/module/user/auth/profile.html',
+    controller: 'profileCtrl',
+    controllerAs: 'profileCtrl',
+    data: {
+      authenticate: true
+    }
+    .state('/newsFeed', {
+      templateUrl: 'script/module/newsFeed/newsFeed.html',
+      controller: 'newsFeedCtrl',
+      controllerAs: 'newsFeedCtrl',
+      data: {
+        authenticate: true
+      }
+    })
+    .state('/charity', {
+      templateUrl: 'script/module/charity/charity.html',
+      controller: 'charityCtrl',
+      controllerAs: 'charityCtrl',
+      data: {
+        authenticate: true
+      }
+    })
+    .state('/challenge', {
+      templateUrl: 'script/module/challenge/challenge.html',
+      controller: 'challengeCtrl',
+      controllerAs: 'challengeCtrl',
+      data: {
+        authenticate: true
+      }
+    })
+    .state('/launch', {
+      templateUrl: 'script/module/challenge/launch.html',
+      controller: 'launchCtrl',
+      controllerAs: 'launchCtrl',
+      data: {
+        authenticate: true
+      }
+    })
+
   }]);
