11fb56f03a97cc0d9712d2e0850399f35d773f21
Kevin Aujla <kevinaujla@gmail.com>
2015-11-13 20:49:28 -0800

    [feat] add challenge view visuals

--- client/script/module/challenge/challenge.js
+++ client/script/module/challenge/challenge.js
@@ -7,18 +7,7 @@ sets up challenge controller
 
 angular.module('App.challenge', ['ui.bootstrap'])
 
-.controller('challengeCtrl', ['$scope', '$window', function($scope, $window) {
-  console.log($scope);
-  console.log($window);
-   $scope.tabs = [
-    { title:'Dynamic Title 1', content:'Dynamic content 1' },
-    { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
-  ];
+.controller('challengeCtrl', [function() {
 
-  $scope.alertMe = function() {
-    setTimeout(function() {
-      $window.alert('You\'ve selected the alert tab!');
-    });
-  };
 
 }]);
