5ded08fcbf5765ccec86de244144fbd02808afac
Kevin Aujla <kevinaujla@gmail.com>
2015-11-13 20:02:10 -0800

    [feat] add angular ui boostrap toggable tabs

--- client/script/module/challenge/challenge.js
+++ client/script/module/challenge/challenge.js
@@ -5,8 +5,20 @@ sets up challenge controller
 
 */
 
-angular.module('App.challenge', [])
+angular.module('App.challenge', ['ui.bootstrap'])
 
-.controller('challengeCtrl', [function() {
+.controller('challengeCtrl', ['$scope', '$window', function($scope, $window) {
+  console.log($scope);
+  console.log($window);
+   $scope.tabs = [
+    { title:'Dynamic Title 1', content:'Dynamic content 1' },
+    { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+  ];
+
+  $scope.alertMe = function() {
+    setTimeout(function() {
+      $window.alert('You\'ve selected the alert tab!');
+    });
+  };
 
 }]);
