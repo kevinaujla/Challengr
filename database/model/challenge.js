d01462185bb858a2e57332a0226c5b331ea7984b
arthur <arthur.mathies@googlemail.com>
2015-11-15 15:27:20 -0800

    [feat]: create challenge data model

--- database/model/challenge.js
+++ database/model/challenge.js
@@ -6,15 +6,22 @@ challenge model/schema
 */
 
 // model for challenge table
-module.exports = function (sequelize, Datatypes) {
+module.exports = function (sequelize, DataTypes) {
   return sequelize.define('challenge', {
-    challenge_response: Datatypes.STRING,
-    challenge_completion: Datatypes.STRING,
-    challenge_title: Datatypes.STRING,
-    challenge_description: Datatypes.STRING,
-    charity_name: Datatypes.STRING,
-    donation_amount: Datatypes.INTEGER,
-    time_frame: Datatypes.DATE
+    title: DataTypes.STRING,
+    type: DataTypes.STRING,
+    description DataTypes.STRING,
+    charity_amount: DataTypes.DECIMAL(10, 2),
+    accepted: DataTypes.BOOLEAN,
+    denied: DataTypes.BOOLEAN,
+    completed: DataTypes.BOOLEAN,
+    not_completed: DataTypes.BOOLEAN,
+    likes: DataTypes.BIGINT,
+    expires: DataTypes.DATE,
+    issued: DataTypes.DATE,
+    completed: DataTypes.DATE,
+    accepted: DataTypes.DATE,
+    denied: DataTypes.DATE
   }, {
     // allows for underscore convention for auto generated properties
     underscore: true
