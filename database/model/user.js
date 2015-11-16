a4725e493f074bbfd6fb1923c79473af9649a63e
arthur <arthur.mathies@googlemail.com>
2015-11-15 17:15:58 -0800

    [refactor]: modify data model requirements

--- database/model/user.js
+++ database/model/user.js
@@ -9,10 +9,22 @@ user model/schema
 module.exports = function (sequelize, DataTypes) {
   return sequelize.define('user', {
     // default size for STRING is 255 bits
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
     // this location data will have to have a 
     // different data type at some point
     // location: DataTypes.STRING,
