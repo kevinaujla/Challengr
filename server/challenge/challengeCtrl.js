51787fd7c45f8bf5d0ade6fda5c133fa8514fcc6
arthur <arthur.mathies@googlemail.com>
2015-11-16 07:04:17 -0800

    [fix]: only import database tables not database

--- server/challenge/challengeCtrl.js
+++ server/challenge/challengeCtrl.js
@@ -5,6 +5,23 @@ configuring routes for challengeRouter
 
  */
 
+// import database models
+var Challenge = db.import(__dirname + '/../../database/model/challenge.js');
+
 module.exports = {
-  
-}
+  create: function (req, res) {
+    // Console Log
+    console.log('/api/challenge/create is being called with body: ' + req.body);
+    // pull out data
+    var challenger = req.user;
+    var challenged = req.body.challenged;
+    var title = req.body.title;
+    var description = req.body.description;
+    var type = req.body.type;
+    var charity_amount = req.body.amount;
+    var issued = new Date();
+    var expires = new Date();
+    // expires in 24 hours
+    expires.setDate(expires.getDate()+1);
+  }
+};
