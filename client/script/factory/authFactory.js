23f98850548a21e295af7dc853ea49a9f3abae6c
Faisal AlQasimi <falqas@gmail.com>
2015-11-15 17:34:18 -0800

    [fix] signup creates correct user entries in db

--- client/script/factory/authFactory.js
+++ client/script/factory/authFactory.js
@@ -5,16 +5,61 @@ handles http request for auth controller
 
 */
 
-angular.module('App.authFactory', [] )
+angular.module('App.authFactory', [])
 
-.factory('AuthFactory', ['$http', '$state', '$window', function($http, $state, $window) {
+.factory('AuthFactory', ['$http', '$state', '$window', '$timeout', function ($http, $state, $window) {
+  var loginorout = "Sign in";
+  var signup = function (user) {
+    return $http({
+        method: 'POST',
+        url: '/api/auth/signup',
+        data: user
+      })
+      .then(function (resp) {
+        return resp.data.token;
+      })
+  };
 
-  var signIn = function(){
+  var signin = function (user) {
+    return $http({
+        method: 'POST',
+        url: '/api/auth/signin',
+        data: user
+      })
+      .then(function (resp) {
+        return resp.data.token;
+      });
+  };
 
+  var signout = function() {
+    $window.localStorage.removeItem('com.challengr');
+    $window.localStorage.removeItem('com.challengr.name');
+    $window.localStorage.removeItem('com.challengr.date')
+      $location.path('/signin');
+  };
+
+  var isAuth = function() {
+    if(!window.localStorage.getItem('com.challengr')){
+      return false;
+    }
+    loginorout = "Logout"
+    if(new Date() - Date.parse($window.localStorage.getItem('com.challengr.date'))>1800000){
+      $window.localStorage.removeItem('com.challengr');
+      $window.localStorage.removeItem('com.challengr.name');
+      $window.localStorage.removeItem('com.challengr.date');
+      return false;
+    }else{
+      $window.localStorage.setItem('com.challengr.date', new Date());
+      return true;
+    }
   };
 
   return {
-    signIn : signIn
+    signup: signup,
+    signin: signin,
+    signout: signout,
+    isAuth: isAuth,
+    loginorout: loginorout
   };
 
 }])
--- client/script/module/user/auth/auth.js
+++ client/script/module/user/auth/auth.js
@@ -7,12 +7,39 @@ sets up authorization controller
 
 angular.module('App.auth', [])
 
-.controller('authCtrl', ['AuthFactory', function (AuthFactory) {
+.controller('authCtrl', function($scope, $window, $location, AuthFactory){
 
   var self = this;
+  self.failedAttempt = false;
+  self.failedLogin = false;
+  self.Loginorout = AuthFactory;
 
-  self.signin = function () {
-    AuthFactory.signIn();
+
+  self.signup = function() {
+    console.log('user object is:', self);
+    AuthFactory.signup(self)
+      .then(function(token) {
+        $window.localStorage.setItem('com.challengr', token);
+      })
+      .catch(function(err) {
+        self.failedAttempt = true;
+        console.log('signup error:', err);
+      });
   };
 
-}]);
+  self.signin = function() {
+    console.log('user signing in:', self);
+      AuthFactory.signin(self)
+        .then(function(token) {
+          AuthFactory.loginorout = "Logout"
+        })
+        .catch(function(err) {
+          self.failedLogin = true;
+          console.log(err);
+        });
+  }
+
+
+
+
+});
