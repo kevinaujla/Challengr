8e330195eadaff45c1761c73445692ef6bab2fe1
Jordan Winkelman <jordanwink201@gmail.com>
2015-11-16 20:56:17 -0800

    [refactor] : nested controller for each step when creating challenge

--- client/script/module/challenge/challenge.js
+++ client/script/module/challenge/challenge.js
@@ -7,127 +7,35 @@ CRUD for challenges
 
 angular.module('App.challenge', ['ui.bootstrap'])
 
-.controller('challengeListCtrl', ['challengeFactory', function (challengeFactory) {
-
-  var self = this;
-
-}])
-
 .controller('challengeNewCtrl', ['createChallengeService', 'challengeFactory', 'braintreeFactory', '$state', function(createChallengeService, challengeFactory, braintreeFactory, $state, $scope) {
 
   var self = this;
 
-  // This is dummy data and needs to be removed once server function works
-  self.friends = [
-    {
-      name : 'Jordan Winkelman',
-      img : 'image/profileImg.jpg'
-    },
-    {
-      name : 'Kevin Aujla',
-      img : 'image/profileImg2.jpg'
-    },
-    {
-      name : 'Faisal Aujla',
-      img : 'image/profileImg2.jpg'
-    },
-    {
-      name : 'Jordan Winkelman',
-      img : 'image/profileImg.jpg'
-    },
-    {
-      name : 'Kevin Aujla',
-      img : 'image/profileImg2.jpg'
-    },
-    {
-      name : 'Faisal Aujla',
-      img : 'image/profileImg2.jpg'
-    }
-  ];
-
-  self.charities = [
-    {
-      name : 'Charity Name',
-      description : 'Charity description and cause',
-      website : 'salvationarmy.com',
-      img : 'image/charity/salvationArmy.png'
-    },
-    {
-      name : 'Charity Name',
-      description : 'Charity description and cause',
-      website : 'salvationarmy.com',
-      img : 'image/charity/salvationArmy.png'
-    },
-    {
-      name : 'Charity Name',
-      description : 'Charity description and cause',
-      website : 'salvationarmy.com',
-      img : 'image/charity/salvationArmy.png'
-    }
-  ];
-
-  self.challengeDescription = '';
-
-  self.payment = {};
-
   /*
-   Steps Tabs
+    Steps Tabs
   */
   self.tabs = [true, false, false];
+  self.currentTab = 0;
   /*
-   Next Step
+    Next Step
   */
   self.nextTab = function(){
-    for (var i = 0; i < self.tabs.length - 1; i++) {
-      if(self.tabs[i] === true){
-        self.tabs[i] = false;
-        if (i < 2) {    
-          self.tabs[++i] = true;
-        }
-      }
+    if (self.currentTab < 2) {
+      console.log('next tab');
+      self.tabs[self.currentTab] = false;
+      self.currentTab++;
+      self.tabs[self.currentTab] = true;
     }
   };
   /*
-   Previous Step
+    Previous Step
   */
   self.prevTab = function(){
-    for (var i = 0; i < self.tabs.length; i++) {
-      if(self.tabs[i] === true){
-        if (i > 0) {  
-          self.tabs[i] = false;  
-          self.tabs[--i] = true;
-        }
-      }
+    if (self.currentTab > 0) {
+      self.tabs[self.currentTab] = false;
+      self.currentTab--;
+      self.tabs[self.currentTab] = true;
     }
-  };
-
-  /*
-    Add selected friend to create challenge service
-  */
-  self.addFriend = function(friend){
-    // console log
-    console.log('add friend to create challenge service : ', friend);
-    // argument to service object
-    createChallengeService.challenge.friendToChallenge = friend;
-    // console log
-    console.log('self.challengeDescription : ', self.challengeDescription);
-  };
-
-  /*
-    Add challenge description, challenge category
-  */
-  self.saveChallengeInfo = function(){
-    // console log
-    console.log('add challenge information...');
-    // 
-    createChallengeService.challenge.description = self.challengeDescription;
-  };
-
-  /*
-    Load all friends from server for the user to filter and choose from for who they want to challenge
-  */
-  self.loadFriends = function(){
-    console.log('loading friends...');
   };
 
   /*
@@ -157,6 +65,138 @@ angular.module('App.challenge', ['ui.bootstrap'])
         console.log('error : ', err);
       });
   };
+
+}])
+
+
+.controller('challengeStep1Ctrl', ['createChallengeService', function(createChallengeService){
+
+  var self = this;
+
+  self.challengeDescription = '';
+
+  // This is dummy data and needs to be removed once server function works
+  self.friends = [
+    {
+      name : 'Jordan Winkelman',
+      img : 'image/profileImg.jpg'
+    },
+    {
+      name : 'Kevin Aujla',
+      img : 'image/profileImg2.jpg'
+    },
+    {
+      name : 'Faisal Aujla',
+      img : 'image/profileImg2.jpg'
+    },
+    {
+      name : 'Jordan Winkelman',
+      img : 'image/profileImg.jpg'
+    },
+    {
+      name : 'Kevin Aujla',
+      img : 'image/profileImg2.jpg'
+    },
+    {
+      name : 'Faisal Aujla',
+      img : 'image/profileImg2.jpg'
+    }
+  ];
+
+  /*
+    Load all friends from server for the user to filter and choose from for who they want to challenge
+  */
+  self.loadFriends = function(){
+    console.log('loading friends...');
+  };
+
+  /*
+    Add selected friend to create challenge service
+  */
+  self.addFriend = function(friend){
+    // console log
+    console.log('add friend to create challenge service : ', friend);
+    // argument to service object
+    createChallengeService.challenge.friendToChallenge = friend;
+    // console log
+    console.log('self.challengeDescription : ', self.challengeDescription);
+  };
+
+  /*
+    Add challenge description, challenge category
+  */
+  self.saveChallengeInfo = function(){
+    // console log
+    console.log('add challenge information...');
+    // 
+    // createChallengeService.challenge.description = self.challengeDescription;
+  };
+
+}])
+
+.controller('challengeStep2Ctrl', ['createChallengeService', function(createChallengeService){
+
+  var self = this;
+
+  self.charities = [
+    {
+      name : 'Charity Name',
+      description : 'Charity description and cause',
+      website : 'salvationarmy.com',
+      img : 'image/charity/salvationArmy.png'
+    },
+    {
+      name : 'Charity Name',
+      description : 'Charity description and cause',
+      website : 'salvationarmy.com',
+      img : 'image/charity/salvationArmy.png'
+    },
+    {
+      name : 'Charity Name',
+      description : 'Charity description and cause',
+      website : 'salvationarmy.com',
+      img : 'image/charity/salvationArmy.png'
+    }
+  ];
+
+  /*
+    
+  */
+  self.saveChallengeInfo = function(){
+    // console log
+    console.log('add challenge information...');
+
+  };
+
+  self.doSomething = function(){
+    console.log('doing something...');
+  };
+
+}])
+
+.controller('challengeStep3Ctrl', ['createChallengeService', function(createChallengeService){
+
+  var self = this;
+
+  self.payment = {};
+
+  /*
+    
+  */
+  self.saveChallengeInfo = function(){
+    // console log
+    console.log('add challenge information...');
+
+  }
+
+}])
+
+
+
+
+.controller('challengeListCtrl', ['challengeFactory', function (challengeFactory) {
+
+  var self = this;
 
 }])
 
