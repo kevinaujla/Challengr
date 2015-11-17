e456314e8b3583b873408f2f422ce1f879addd46
Kevin Aujla <kevinaujla@gmail.com>
2015-11-16 16:49:32 -0800

    [feat] add radio controller for sharing radio button functionality

--- client/script/module/challenge/challenge.js
+++ client/script/module/challenge/challenge.js
@@ -53,28 +53,6 @@ angular.module('App.challenge', ['ui.bootstrap'])
 
 }])
 
-.controller('ButtonsCtrl',['$scope', function ($scope) {
-
-  $scope.radioModel = 'Middle';
-
-  $scope.checkModel = {
-    left: false,
-    middle: true,
-    right: false
-  };
-
-  $scope.checkResults = [];
-
-  $scope.$watchCollection('checkModel', function () {
-    $scope.checkResults = [];
-    angular.forEach($scope.checkModel, function (value, key) {
-      if (value) {
-        $scope.checkResults.push(key);
-      }
-    });
-  });
-}])
-
 .controller('challengeEditCtrl', ['ChallengeFactory', function (ChallengeFactory) {
 
   var self = this;
---
+++ client/script/module/radioButton/radioButton.js
@@ -0,0 +1,23 @@
+angular.module('App.radio', [])
+
+.controller('ButtonsCtrl',['$scope', function ($scope) {
+
+  $scope.radioModel = 'Middle';
+
+  $scope.checkModel = {
+    left: false,
+    middle: true,
+    right: false
+  };
+
+  $scope.checkResults = [];
+
+  $scope.$watchCollection('checkModel', function () {
+    $scope.checkResults = [];
+    angular.forEach($scope.checkModel, function (value, key) {
+      if (value) {
+        $scope.checkResults.push(key);
+      }
+    });
+  });
+}]);
