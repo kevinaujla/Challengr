dd77a38e5e3843d543c06d2254902cab97fb5f31
Jordan Winkelman <jordanwink201@gmail.com>
2015-11-17 17:23:44 -0800

    [feat] : setup challenge incomplete and complete directives

--- client/script/directive/challenge/challengeDirective.js
+++ client/script/directive/challenge/challengeDirective.js
@@ -7,7 +7,7 @@ challengeDirective.js
 
 angular.module('App.challengeDirective', [])
 
-.directive('challenge', ['$moment', 'challengeFactory', function(challengeFactory, $moment){
+.directive('challenge', ['challengeFactory', function(challengeFactory){
 
   return {
     restrict : 'E',
@@ -45,11 +45,11 @@ angular.module('App.challengeDirective', [])
           likes : ++scope.likes,
           completed : scope.completed,
           notCompleted : scope.notcompleted,
-        }
+        };
         console.log('obj : ', updateObj);
         // call factory function to update challenge values
         challengeFactory.updateChallenge(updateObj)
-          .then(function(data){
+          .then(function(){
             console.log('succesfully increased like...');
           })
           .catch(function(err){
@@ -62,38 +62,50 @@ angular.module('App.challengeDirective', [])
 
 }])
 
-.directive('completed', function(){
-
-  return {
-    restrict : 'E',
-    templateUrl: 'script/directive/challenge/challengeCDirective.html',
-    scope : { 
-
-    },
-    link : function($scope, element, attrs){
-      element.bind('click', function(){
-        console.log('completed challenge...');
-      });
-    }
-  };
-
-})
-
-.directive('incomplete', function(){
+.directive('incomplete', ['challengeFactory', function(challengeFactory){
 
   return {
     restrict : 'E',
     templateUrl: 'script/directive/challenge/challengeICDirective.html',
     scope : { 
-
+      challengeid : '=',
+      charityamount : '=',
+      title : '=',
+      description : '=',
+      challenged : '=',
+      likes : '=',
+      completeddate : '=',
+      expiresdate : '=',
+      issueddate : '=',
+      completed : '=',
+      notcompleted : '=',
+      type : '=',
     },
-    link : function($scope, element, attrs){
-      element.bind('click', function(){
-        console.log('incomplete challenge...');
-      });
+    link : function(scope, element, attrs){
+
+      scope.increaseLike = function(){
+        // console log
+        console.log('increase like...');
+        // create object to update
+        var updateObj = {
+          id : scope.challengeid,
+          likes : ++scope.likes,
+          completed : scope.completed,
+          notCompleted : scope.notcompleted,
+        };
+        console.log('obj : ', updateObj);
+        // call factory function to update challenge values
+        challengeFactory.updateChallenge(updateObj)
+          .then(function(){
+            console.log('succesfully increased like...');
+          })
+          .catch(function(err){
+            console.log('error increasing like : ', err);
+          });
+      };
     }
   };
 
-})
+}]);
 
 
