896badf4d604323278245e1b353f99a5a7482ca1
Jordan Winkelman <jordanwink201@gmail.com>
2015-11-17 16:33:54 -0800

    [feat] : increase likes and displays charity amount

--- client/script/directive/challenge/challengeDirective.js
+++ client/script/directive/challenge/challengeDirective.js
@@ -7,12 +7,14 @@ challengeDirective.js
 
 angular.module('App.challengeDirective', [])
 
-.directive('challenge', function(){
+.directive('challenge', ['$moment', 'challengeFactory', function(challengeFactory, $moment){
 
   return {
     restrict : 'E',
     templateUrl: 'script/directive/challenge/challengeDirective.html',
     scope : { 
+      challengeid : '=',
+      charityamount : '=',
       title : '=',
       description : '=',
       challenged : '=',
@@ -24,24 +26,41 @@ angular.module('App.challengeDirective', [])
       notcompleted : '=',
       type : '=',
     },
-    link : function($scope, element, attrs){
+    link : function(scope, element, attrs){
       // Need to fire an event when the issueDate moment difference passes the expiresdate
       // event should change the state of the challenge to become incomplete
+  
       // element.bind('click', function(){
       //   console.log('scope : ', scope.issueddate);
       //   console.log('attrs : ', attrs.expiresdate);  
       //   console.log('element : ', element);
       // });
 
-      $scope.increaseLike = function(){
+      scope.increaseLike = function(){
+        // console log
         console.log('increase like...');
-        
+        // create object to update
+        var updateObj = {
+          id : scope.challengeid,
+          likes : ++scope.likes,
+          completed : scope.completed,
+          notCompleted : scope.notcompleted,
+        }
+        console.log('obj : ', updateObj);
+        // call factory function to update challenge values
+        challengeFactory.updateChallenge(updateObj)
+          .then(function(data){
+            console.log('succesfully increased like...');
+          })
+          .catch(function(err){
+            console.log('error increasing like : ', err);
+          });
       };
 
     }
   };
 
-})
+}])
 
 .directive('completed', function(){
 
