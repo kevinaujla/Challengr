eafb7fbb5432db1239425103f1b9a7350d9d1bfc
Jordan Winkelman <jordanwink201@gmail.com>
2015-11-19 18:41:42 -0800

    [refactor] : adding $interval to check the expiration time of challenge

--- client/script/module/user/challenge/personalChallenge.js
+++ client/script/module/user/challenge/personalChallenge.js
@@ -7,9 +7,21 @@ personalChallenge.js
 
 angular.module('App.personalChallenge', [])
 
-.controller('personalChallengeCtrl', ['challengeFactory', function(challengeFactory) {
+.controller('personalChallengeCtrl', ['challengeFactory', '$scope', '$interval', function(challengeFactory, $scope, $interval) {
 
   var self = this;
+
+  $scope.date = null;
+
+  $scope.$watch('date | json', function() {
+    console.log('date : ', $scope.date);
+    // every minute do a check if the date has reached the expired date
+    
+  });
+  
+  $interval(function() {
+    $scope.date = moment(new Date());
+  }, 60000);
 
   self.challenges = [];
 
