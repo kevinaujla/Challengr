481294588c661bd0bbb09091f322a56421bff96f
Jordan Winkelman <jordanwink201@gmail.com>
2015-12-03 11:52:10 -0800

    [feat]: using aws database

--- database/database.js
+++ database/database.js
@@ -10,24 +10,19 @@ var sequelize = require('sequelize');
 
 // instantiates a new Sequelize database
 var db = new sequelize(
-  // databasa username and password do not matter for sqlite
-  'database',
-  'username',
-  'password', {
-    host: 'localhost',
-    dialect: 'sqlite',
-    // the pool size depends on the load the database has to handle
-    // as long as this is sqlite the number can be marginally small
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD, {
+    host: process.env.DATABASE_URL,
+    port: process.env.DATABASE_PORT,
+    dialect: 'mysql',
     pool: {
       max: 10,
       min: 2,
-      // milliseconds being idle before released
       idle: 100000
     },
-    storage: __dirname + '/data/db.sqlite',
-    logging : false,
+    logging: false
   });
-
 require(__dirname + '/configDatabase.js')(db);
 
 module.exports = db;
