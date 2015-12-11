3b9adbed2883377b56ebc1e1458cf40c2883ace9
Jordan Winkelman <jordanwink201@gmail.com>
2015-12-11 10:28:48 -0800

    [fix]: cannot see challenges sent if they are not active

--- client/script/module/challenge/challenge-view.js
+++ client/script/module/challenge/challenge-view.js
@@ -8,55 +8,55 @@ Challenges in newsfeed view
 angular.module('App.challengeView', [])
 
 .controller('challengeViewCtrl', ['challengeFactory', '$stateParams', '$state', 'challengeService', 'alertService', '$rootScope', function (challengeFactory, $stateParams, $state, challengeService, alertService, $rootScope) {
-  
- var self = this;
- self.challenge = null;
- self.isCurrentUserChallenge = false;
- var currentUserEmail = localStorage.getItem('com.challengr.email');
 
- // Read the challenge id from the service object to retreive the correct challenge by it's ID
- self.readChallenge = function () {
-   for (var i = 0; i < challengeService.challenges.length; i++) {
-     if (challengeService.challenges[i].id == $stateParams.id) {
-       self.challenge = challengeService.challenges[i];
-       if (currentUserEmail === self.challenge.Challenged.email && !self.challenge.notCompleted && !self.challenge.completed) {
-         self.isCurrentUserChallenge = true;
-       }
-       return;
-     }
-   }
- };
+  var self = this;
+  self.challenge = null;
+  self.isCurrentUserChallenge = false;
+  var currentUserEmail = localStorage.getItem('com.challengr.email');
 
-  // Set Globals back to true
-  self.homeSetDetail = function(){
-   $rootScope.globalLeftDetailView = true;
-   $rootScope.globalRightDetailView = true;
-   $rootScope.leftDetailViewRoute = false;
-   $state.transitionTo('home');
+  // Read the challenge id from the service object to retreive the correct challenge by it's ID
+  self.readChallenge = function () {
+    for (var i = 0; i < challengeService.challenges.length; i++) {
+      if (challengeService.challenges[i].id == $stateParams.id) {
+        self.challenge = challengeService.challenges[i];
+        if (currentUserEmail === self.challenge.Challenged.email && !self.challenge.notCompleted && !self.challenge.completed) {
+          self.isCurrentUserChallenge = true;
+        }
+        return;
+      }
+    }
   };
 
- self.increaseLike = function () {
-   var updateObj = {
-     id: self.challenge.id,
-     likes: ++self.challenge.likes
-   };
-   challengeFactory.updateChallenge(updateObj)
-     .catch(function (err) {
-       console.log('error increasing like : ', err);
-     });
- };
+  // Set Globals back to true
+  self.homeSetDetail = function () {
+    $rootScope.globalLeftDetailView = true;
+    $rootScope.globalRightDetailView = true;
+    $rootScope.leftDetailViewRoute = false;
+    $state.transitionTo('home');
+  };
 
- self.completed = function () {
-   self.challenge.completed = true;
-   alertService.addAlert('success', 'Successfuly marked challenge completed', 'icon-checkbox');
-   var updateObj = {
-     id: self.challenge.id,
-     completed: true
-   };
-   challengeFactory.updateChallenge(updateObj)
-     .catch(function (err) {
-       console.log('error updating completion status');
-     });
- };
+  self.increaseLike = function () {
+    var updateObj = {
+      id: self.challenge.id,
+      likes: ++self.challenge.likes
+    };
+    challengeFactory.updateChallenge(updateObj)
+      .catch(function (err) {
+        console.log('error increasing like : ', err);
+      });
+  };
+
+  self.completed = function () {
+    self.challenge.completed = true;
+    alertService.addAlert('success', 'Successfuly marked challenge completed', 'icon-checkbox');
+    var updateObj = {
+      id: self.challenge.id,
+      completed: true
+    };
+    challengeFactory.updateChallenge(updateObj)
+      .catch(function (err) {
+        console.log('error updating completion status');
+      });
+  };
 
 }]);
